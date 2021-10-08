7044d95900c0d9f330cb086b6da52e633690776b
Francisco Garcia <fgarcia.student04@gmail.com>
2021-10-08 18:13:36 -0400

    removing configservice, adding dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnimalModule } from './animal/animal.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AnimalModule.register()],
+  imports: [AnimalModule.register()],
   controllers: [AppController],
   providers: [AppService],
 })
